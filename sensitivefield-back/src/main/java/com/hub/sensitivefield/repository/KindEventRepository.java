e4b39fddcfb1b6f379b85e111e098c144340c4c0
michael <m.nawrocki@i.ua>
2020-11-12 21:58:39 +0200

    ref(back): delete 'public' modifier of methods' signatures in interfaces

--- sensitivefield-back/src/main/java/com/hub/sensitivefield/repository/KindEventRepository.java
+++ sensitivefield-back/src/main/java/com/hub/sensitivefield/repository/KindEventRepository.java
@@ -9,7 +9,9 @@ import java.util.List;
 
 @Repository
 public interface KindEventRepository extends JpaRepository<KindEvent, Integer> {
-    public KindEvent findByName(String name);
-    public List<KindEvent> findAllByPriority (String priority);
-    public List<KindEvent> findAllByTypeEvent(TypeEvent typeEvent);
+    KindEvent findByName(String name);
+
+    List<KindEvent> findAllByPriority(String priority);
+
+    List<KindEvent> findAllByTypeEvent(TypeEvent typeEvent);
 }
